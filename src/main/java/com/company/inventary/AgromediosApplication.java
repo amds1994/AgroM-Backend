cf0fd0b707f4db1ca112f34657fc2683556ffdf7
Andres Mauricio Dominguez Sanchez <amds1994@gmail.com>
2023-03-02 14:39:58 -0500

    proyecto base de control de inventario

---
+++ src/main/java/com/company/inventary/AgromediosApplication.java
@@ -0,0 +1,13 @@
+package com.company.inventary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgromediosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgromediosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/inventary/AgromediosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.inventary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgromediosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
