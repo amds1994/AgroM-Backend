3cbcb869e02960fb43476a80dec890886e765393
Andres Mauricio Dominguez Sanchez <amds1994@gmail.com>
2023-03-03 21:51:41 -0500

    agregada la funcion de gurdar categoria

--- src/main/java/com/company/inventary/controller/CategoryRestController.java
+++ src/main/java/com/company/inventary/controller/CategoryRestController.java
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.company.inventary.model.Category;
 import com.company.inventary.response.CategoryResponseRest;
 import com.company.inventary.services.ICategoryService;
 
@@ -36,6 +39,18 @@ public class CategoryRestController {
 	public ResponseEntity<CategoryResponseRest> searchCategoryById(@PathVariable Long id) {
 		
 		ResponseEntity<CategoryResponseRest> response = service.searchById(id);
+		return response;
+	}
+	
+	/**
+	 * Save categories
+	 * @param Category
+	 * @return
+	 */
+	@PostMapping("/categories")
+	public ResponseEntity<CategoryResponseRest> save(@RequestBody Category category) {
+		
+		ResponseEntity<CategoryResponseRest> response = service.save(category);
 		return response;
 	}
 
--- src/main/java/com/company/inventary/services/ICategoryService.java
+++ src/main/java/com/company/inventary/services/ICategoryService.java
@@ -2,11 +2,13 @@ package com.company.inventary.services;
 
 import org.springframework.http.ResponseEntity;
 
+import com.company.inventary.model.Category;
 import com.company.inventary.response.CategoryResponseRest;
 
 public interface ICategoryService {
 	
 	public ResponseEntity<CategoryResponseRest> search();
 	public ResponseEntity<CategoryResponseRest> searchById(Long id);
+	public ResponseEntity<CategoryResponseRest> save(Category category);
 
 }
