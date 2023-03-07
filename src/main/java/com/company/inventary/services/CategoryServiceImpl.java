4556ff661f2bf8cf48e782dfbd5f0b824adc00f1
Andres Mauricio Dominguez Sanchez <amds1994@gmail.com>
2023-03-06 20:29:21 -0500

    Servicio de eliminar categorias

--- src/main/java/com/company/inventary/services/CategoryServiceImpl.java
+++ src/main/java/com/company/inventary/services/CategoryServiceImpl.java
@@ -144,4 +144,26 @@ public class CategoryServiceImpl implements ICategoryService{
 		return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
 	}
 
+	@Override
+	@Transactional
+	public ResponseEntity<CategoryResponseRest> deleteById(Long id) {
+		
+		CategoryResponseRest response = new CategoryResponseRest();
+		
+		try {
+			
+			categoryDao.deleteById(id);
+			response.setMetadata("Respuesta OK", "00", "Registro eliminado");
+			
+		} catch (Exception e) {
+			response.setMetadata("Respuesta no OK", "-1", "Error al eliminar");
+			e.getStackTrace();
+			return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+			
+		}
+		
+		return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
+		
+	}
+
 }
