412873b22d2847e8b1f1e57c808f8049f2771511
Andres Mauricio Dominguez Sanchez <amds1994@gmail.com>
2023-03-06 20:37:01 -0500

    Agregado el CORS

--- src/main/java/com/company/inventary/controller/CategoryRestController.java
+++ src/main/java/com/company/inventary/controller/CategoryRestController.java
@@ -2,6 +2,7 @@ package com.company.inventary.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import com.company.inventary.model.Category;
 import com.company.inventary.response.CategoryResponseRest;
 import com.company.inventary.services.ICategoryService;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api/v1")
 public class CategoryRestController {
