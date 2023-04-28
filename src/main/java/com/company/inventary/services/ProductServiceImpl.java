ff8f5e5993721d4ebccd0a8deabf18f852e009aa
Andres Mauricio Dominguez Sanchez <amds1994@gmail.com>
2023-04-27 22:25:41 -0500

    Buscar productos agregado

--- src/main/java/com/company/inventary/services/ProductServiceImpl.java
+++ src/main/java/com/company/inventary/services/ProductServiceImpl.java
@@ -172,4 +172,45 @@ public class ProductServiceImpl implements IProductService {
 		return new ResponseEntity<ProductResponseRest>(response, HttpStatus.OK);
 	}
 
+
+	@Override
+	@Transactional(readOnly = true)
+	public ResponseEntity<ProductResponseRest> search() {
+		
+		ProductResponseRest response = new ProductResponseRest();
+		List<Product> list = new ArrayList<>();
+		List<Product> listAux = new ArrayList<>();
+		
+		try {
+			
+			// Buscar productos
+			listAux = (List<Product>) productDao.findAll();
+			
+			if (listAux.size() > 0) {
+				
+				listAux.stream().forEach( (p) -> {
+					byte[] imageDescompressed = Util.decompressZLib(p.getPicture());
+					p.setPicture(imageDescompressed);
+					list.add(p);
+				});
+				
+				
+				response.getProduct().setProducts(list);
+				response.setMetadata("Respuesta Ok", "00", "Productos encontrados");
+				
+			} else {
+				response.setMetadata("Respuesta no Ok", "-1", "Productos no encotrados");
+				return new ResponseEntity<ProductResponseRest>(response, HttpStatus.NOT_FOUND);
+			}
+			
+			
+		} catch (Exception e) {
+			e.getStackTrace();
+			response.setMetadata("Respuesta no Ok", "-1", "Error al buscar productos");
+			return new ResponseEntity<ProductResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		return new ResponseEntity<ProductResponseRest>(response, HttpStatus.OK);
+	}
+
 }
